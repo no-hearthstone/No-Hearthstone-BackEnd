f28668dcec2650d06fa9c39f11de75ed435a53d4
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2024-07-18 15:57:16 +0900

    :tada:

---
+++ No-Hearthstone/src/main/kotlin/com/nohearthstone/No/Hearthstone/NoHearthstoneApplication.kt
@@ -0,0 +1,11 @@
+package com.nohearthstone.No.Hearthstone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoHearthstoneApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoHearthstoneApplication>(*args)
+}
---
+++ No-Hearthstone/src/test/kotlin/com/nohearthstone/No/Hearthstone/NoHearthstoneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nohearthstone.No.Hearthstone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoHearthstoneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
